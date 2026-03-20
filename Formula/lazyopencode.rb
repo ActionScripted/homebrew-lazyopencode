5caba318c1c3f7dda2998a5340f009fdcf083e77
Michael Thompson <actionscripted@gmail.com>
2026-03-20 11:24:21 -0400

    chore: update lazyopencode to v0.0.3

--- Formula/lazyopencode.rb
+++ Formula/lazyopencode.rb
@@ -3,18 +3,18 @@
 class Lazyopencode < Formula
   desc "Terminal UI for managing opencode sessions"
   homepage "https://github.com/actionscripted/lazyopencode"
-  version "0.0.2"
+  version "0.0.3"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/actionscripted/lazyopencode/releases/download/v#{version}/lazyopencode-darwin-amd64"
-      sha256 "PLACEHOLDER_AMD64_SHA256"
+      sha256 "c0cd72a08ae5211ec0545d4fa4c4ce10f5db009a14bed24e265685cb563898c1"
     end
 
     on_arm do
       url "https://github.com/actionscripted/lazyopencode/releases/download/v#{version}/lazyopencode-darwin-arm64"
-      sha256 "PLACEHOLDER_ARM64_SHA256"
+      sha256 "e53bda68fee3ab6275d693961c691eb574bf83b73b88b7c00add4d1bcabd7066"
     end
   end
 
